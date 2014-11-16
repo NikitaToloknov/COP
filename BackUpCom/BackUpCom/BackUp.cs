4653cd9f9b442c9be3efcdd5e302e3ae63c9a851
Nikita Toloknov <nik.toloknov@yandex.ru>
2014-11-16 14:21:09 +0300

    Create component BackUp

---
+++ BackUpCom/BackUpCom/BackUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackUpCom
+{
+    public partial class BackUp : Component
+    {
+        public BackUp()
+        {
+            InitializeComponent();
+        }
+
+        public BackUp(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
